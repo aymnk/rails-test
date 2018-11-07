e7f926c019b88975e6b172da23eea6832b369ce0
aigrec <xaigrec@gmail.com>
2018-11-07 22:12:13 +0900

    tutorial
    Ruby on rails tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'foo/home'
+  get 'foo/help'
+  get 'foo/baz'
+  get 'static_pages/home'
+  get 'static_pages/help'
+  resources :microposts
+  resources :users
+  root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
